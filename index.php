5b111bd79af082ad5721876ade8801e33ebd000d
manuelgaleone <galeonemanuel14@gmail.com>
2022-12-07 14:56:35 +0100

    Initial commit - Created the class

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+Definita una classe ‘Movie’
+
+    all'interno della classe sono dichiarate delle variabili d'istanza
+    all'interno della classe è definito un costruttore
+    all'interno della classe è definito almeno un metodo
+    
+    vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+*/
+
+class Movie
+{
+    public $title;
+    public $genre;
+    public $year;
+    public $author;
+}
